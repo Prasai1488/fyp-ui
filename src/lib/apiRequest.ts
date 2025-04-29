d567b637e72f70d75f2cad7472147065d1022947
Prasai1488 <prasaiprithvi4@gmail.com>
2025-04-29 11:07:20 +0545

    base url updated

--- src/lib/apiRequest.ts
+++ src/lib/apiRequest.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8090/api",
+  // baseURL: "http://localhost:8090/api",
+  baseURL: "https://gorkhahomes-backend.onrender.com/api",
   withCredentials: true,
 });
 
